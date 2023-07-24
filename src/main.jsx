c8152c3d96484cc5843d3789c0b9b05d652c7f02
SxmBetosk8 <alan_06.04@hotmail.cl>
2023-07-23 23:19:25 -0400

    Se añadió Tareas en agregar y listar (falta modificar), inclusión de bootstrap 5.3.0

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Tareas from './components/tareas/Tareas.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+
+  },
+  {
+    path: "/Tareas",
+    element: <Tareas />,
+
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
